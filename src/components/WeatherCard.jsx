f3634f4270a78a225d18f88b14c912b7570137a4
Diego <service.dev92@gmail.com>
2022-09-11 05:17:50 -0300

    Refactor cards pronostico extendido

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,21 +1,56 @@
-import { format } from "date-fns";
 import { getFormatBy } from "../utils/dateUtils";
 
 export const WeatherCard = ({ weatherHour }) => {
-  const { hour } = weatherHour;
+  const {
+    id,
+    atmosphericPressure,
+    hour,
+    humidity,
+    icon,
+    temperature,
+    thermalSensation,
+  } = weatherHour;
   return (
-    <div className="card mb-3">
-      <h5 className="card-header">
+    <div className="card mb-3 shadow">
+      <h5 className="card-header text-center">
         {`${getFormatBy("day", hour)} | ${getFormatBy("hour", hour)} hs.`}
       </h5>
+
+      <div className="container mt-2">
+        <div className="row align-items-center p-3">
+          <div className="col">
+            <img
+              src={`http://openweathermap.org/img/w/${icon}.png`}
+              className="rounded img-thumbnail"
+              height="0.3rem"
+              alt={`${icon}_${id}`}
+            />
+          </div>
+          <div className="col">
+            <h2 className="m-0 text-end">{temperature.toFixed(1)}°C</h2>
+          </div>
+        </div>
+      </div>
+
       <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <div className="container mt-2">
+          <div className="row align-items-center text-center">
+            <div className="col">
+              <p className="m-0">ST: {thermalSensation.toFixed(1)}°C</p>
+            </div>
+            <div className="col">
+              <p className="m-0">Hum: {humidity}%</p>
+            </div>
+          </div>
+          <hr />
+          <div className="row align-items-center text-center">
+            <div className="col">
+              <p className="m-0">
+                Pres AT: {atmosphericPressure} Pa
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/helpers/getWeatherForecast.js
+++ src/helpers/getWeatherForecast.js
@@ -1,5 +1,5 @@
 export const getWeatherForecast = async (city) => {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=-34.57&lon=-58.42&appid=9a906dce0682400c53780ed9cc8f3bfd`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=-34.57&lon=-58.42&units=metric&lang=es&appid=9a906dce0682400c53780ed9cc8f3bfd`;
   const resp = await fetch(url);
   const data = await resp.json();
   const hourlyForecast = data.list.map((hour) => ({
